715cc27462b83970e1f08469ac2f83571bbba740
Aaron King <aaronking@LTP-003.local>
2020-03-05 12:07:01 +0000

    setting up react app with routes and dependendices

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "note-app-uploads13"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://q74pj6prcb.execute-api.us-east-2.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_ApH8IboNf",
+      APP_CLIENT_ID: "ksl9gq564nklhdha1h5pvj5m5",
+      IDENTITY_POOL_ID: "us-east-2:43d8d1f6-f98a-4d33-86c7-362f7a2ce84d"
+    }
+  };
